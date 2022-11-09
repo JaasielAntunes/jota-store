dee8de5e829ebebe3b051d60308d76eef30ff0f5
Jaasiel <jasiel.araujo098@gmail.com>
2022-11-09 11:33:28 -0300

    Banco de dados H2 e criação automática da base de dados

--- src/main/java/com/antunes/jotastore/domain/Categoria.java
+++ src/main/java/com/antunes/jotastore/domain/Categoria.java
@@ -4,16 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.UUID;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
 public class Categoria implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+
     private String nome;
 }
